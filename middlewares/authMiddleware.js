f3e31ab5a2cc1c3b55ae3134a1012606f1bbe557
VladymereBilan <vladymeresbilan@gmail.com>
2025-11-16 16:50:11 +0800

    Added console.log

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,6 +4,8 @@
 const apiKeyMiddleware = (req, res, next) => {
   const apiKey = req.header('API_KEY');  // Look for the API key in the 'API_KEY' header
 
+  console.log(apiKey);
+
   // Check if the API key from the request matches the environment variable
   if (!apiKey || apiKey !== process.env.API_KEY) {  // Use the correct environment variable name
     return res.status(401).json({ message: 'Unauthorized: Invalid or missing API key' });
